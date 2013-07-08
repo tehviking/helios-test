498f0bfe88e82e839b91c7f1b74703dfd8c8c3ad
Brandon Hays <brandon.hays@gmail.com>
2013-07-07 23:38:24 -0500

    add upstart task to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,16 +24,37 @@ role :app, "198.61.231.71" # This may be the same as your `Web` server
 role :db, "198.61.231.71" , :primary => true # This is where Rails migrations will run
 
 namespace :deploy do
-  # task :restart_passenger do
-  #   run "touch #{current_path}/tmp/restart.txt"
-  # end
-
   desc "Symlink shared configs and folders on each release."
   task :symlink_shared do
     run "ln -nfs #{shared_path}/.env #{release_path}/.env"
   end
-
 end
 
+
+namespace :foreman do
+  desc "Export the Procfile to Ubuntu's upstart scripts"
+  task :export, :roles => :app do
+    run "cd /home/#{user}/#{application} && sudo bundle exec foreman export upstart /etc/init -a #{application} -u deploy -l /var/#{application}/log"
+  end
+  
+  desc "Start the application services"
+  task :start, :roles => :app do
+    sudo "start #{application}"
+  end
+
+  desc "Stop the application services"
+  task :stop, :roles => :app do
+    sudo "stop #{application}"
+  end
+
+  desc "Restart the application services"
+  task :restart, :roles => :app do
+    run "sudo start #{application} || sudo restart #{application}"
+  end
+end
+
+after "deploy:update", "foreman:export"
+after "deploy:update", "foreman:restart"
+
 after 'deploy:update_code', 'deploy:symlink_shared'
-after "deploy:restart", "deploy:cleanup"
+after "foreman:restart", "deploy:cleanup"
